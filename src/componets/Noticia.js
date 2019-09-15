fcb2207b3efb522b923f62f64012850709e4cf9d
northerchild <osarus-13@hotmail.com>
2019-09-15 16:18:41 -0500

    Se crea el sistema de cards

--- src/componets/Noticia.js
+++ src/componets/Noticia.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-const Noticia = ()=>{
+const Noticia = ({noticia})=>{
+    //extraer los datos
+
+    const {urlToImage, url, title, description, source} = noticia;
+
     return(
-     <h1>Desde Noticias</h1>
+     <div className="col s12 m6 l4">
+         <div className="card">
+             <div className="card-image">
+                 <img src={urlToImage} alt={title}/>
+             </div>
+         </div>
+         <div className="card-content">
+                <h3>{title}</h3>
+                <p>{description}</p>
+         </div>
+         <div className="card-action">
+            <a href={url} target="_blank" className="waves-effect waves-light btn">
+                Ver Noticia Completa
+            </a>
+         </div>
+     </div>
     );
 }
 
