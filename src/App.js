f717b29c60ccae9e4e7175c0a4f3842940f97ecd
northerchild <osarus-13@hotmail.com>
2019-09-15 14:41:19 -0500

    Se conecta con la API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  state={
+    noticias: []
+  }
+
+  componentDidMount(){
+      this.consultarNoticias();
+  }
+
+  consultarNoticias = async ()=>{
+    const url = `https://newsapi.org/v2/top-headlines?country=co&category=business&apiKey=2556e9f5aa524b7691e6cf30422786f8`
+    const respuestas = await fetch(url);
+    const noticias = await respuestas.json()
+    console.log(noticias.articles);
+    this.setState({
+      noticias : noticias.articles
+    })
+  }
+
+  render(){
+    return(<h1>Noticias React</h1>)
+  }
 }
-
 export default App;
